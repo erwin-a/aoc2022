a32b580443b9c921678c0c7ab1920065ee2a7ba6
Erwin S. Andreasen <erwin.andreasen@forsta.com>
2022-12-22 22:12:36 +0100

    Day 21

--- day21.py
+++ day21.py
@@ -19,6 +19,7 @@ ops = {
     '/': my_div
 }
 
+
 def read(n):
     resolved, func = {}, {}
     for x in tokens(n):
@@ -31,21 +32,18 @@ def read(n):
 
 
 def simplify(func, resolved):
-    reduced = False
-    for name, (op, left, right) in list(func.items()):
-        try:
-            resolved[name] = op(resolved[left], resolved[right])
-            del func[name]
-            reduced = True
-        except KeyError as e:
-            pass
-    return reduced
+    while "root" not in resolved:
+        for name, (op, left, right) in list(func.items()):
+            try:
+                resolved[name] = op(resolved[left], resolved[right])
+                del func[name]
+            except KeyError:
+                pass
 
 
 def main():
     resolved, func = read("21")
-    while "root" not in resolved:
-        simplify(func, resolved)
+    simplify(func, resolved)
     print(resolved["root"])
 
 
@@ -53,14 +51,10 @@ def main2():
     resolved, func = read("21")
     m = Model()
 
-    func["root"] = operator.eq, *func["root"][1:]
-    X = resolved["humn"] = intvar(lb=-2 ** 50, ub=2 ** 50) # manually chosen bounds for the constraint solver
+    func["root"] = (operator.eq, *func["root"][1:])
+    X = resolved["humn"] = intvar(lb=-2 ** 50, ub=2 ** 50)  # manually chosen bounds for the constraint solver
 
-    while "root" not in resolved:
-        simplify(func, resolved)
-
-    # in my test case, the target formula is:
-    # (sum([119, 389, 2 * (63484871944429 + (-((317 + (3 * (-708 + ((sum([595, -776, 3 * (84 + ((sum([502, -735, 2 * (874 + (34 * (-551 + ((778 + ((582 + (2 * (-264 + (2 * (106 + ((-598 + ((424 + (2 * (-24 + ((sum([491, -575, 2 * (989 + (9 * (-905 + ((663 + (40 * (-940 + ((588 + (2 * (-916 + ((306 + (5 * (26 + ((-227 + ((930 + (2 * (-581 + ((660 + (9 * (-247 + (16 * (-378 + ((903 + (IV0)) / 5)))))) / 3)))) / 2)) / 2)))) / 7)))) / 12)))) / 11))))])) / 2)))) / 2)) / 3)))))) / 5)) / 5))))])) / 9))])) / 4)))) / 2)))])) / 2 == 30328243757936
+    simplify(func, resolved)    # help the solver a bit
 
     m += resolved["root"]
     assert m.solve()
